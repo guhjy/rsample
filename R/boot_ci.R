bb7ae1c4ecdc9bb8a77eb71345ceaad6c8dee444
fbchow <fannybchow@gmail.com>
2018-06-19 11:20:02 -0700

    Add back BCa methods

--- R/boot_ci.R
+++ R/boot_ci.R
@@ -46,6 +46,39 @@ boot_ci_perc <- function(bt_resamples, var, alpha, data = NULL, theta_obs) {
 }
 
 
+# TO-DO return tibble with upper lower alpha
+boot_ci_bca <- function(bt_resamples, alpha, data){
+  theta_hat = mean(bt_resamples$theta_i)
+
+  ### Estimating Z0:
+  po = mean(bt_resamples$theta_i <= theta_hat)
+  Z0 = qnorm(po)
+  Za = qnorm(1-alpha/2)
+
+  # // TO-DO I'm sure you can do LOO sampling using rsample()...akin to LOO CV
+  # loo_rsets <- loo_cv(as_tibble(data))
+  # loo_df <- loo_rsets %>%
+  #   mutate(theta_i =  mean())
+  # Error: C stack usage  7970176 is too close to the limit
+
+  leave_one_out_theta = sapply(1:length(data), function(i){
+    leave_out_data = data[-i] # leave out the ith observation
+    theta_i = mean(leave_out_data)
+    return(theta_i)
+  })
+
+  theta_minus_one = mean(leave_one_out_theta)
+  a = sum( (theta_minus_one - leave_one_out_theta)^3)/( 6 *(sum( (theta_minus_one - leave_one_out_theta)^2))^(3/2) )
+
+  Zu = (Z0+Za)/(1-a*(Z0+Za)) + Z0 # upper limit for Z
+  Zl = (Z0-Za)/(1-a*(Z0-Za)) + Z0 # Lower limit for Z
+  lower_percentile = pnorm(Zl,lower.tail = TRUE) # percentile for Z
+  upper_percentile = pnorm(Zu,lower.tail = TRUE) # percentile for Z
+  ci_bca = as.numeric(quantile(bt_resamples$theta_i, c(lower_percentil, upper_percentile))) # putting those percentiles in place of alpha/2, 1-alpha/
+  return(ci_bca)
+}
+
+
 
 
 
